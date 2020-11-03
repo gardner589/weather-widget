3064f26b490f13fba9e189f35922dce40112c35b
Chris Gardner <ctgardner89@gmail.com>
2020-11-03 14:23:04 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Container/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import Container from './components/Container'
+
+export default {
+  name: 'App',
+  components: {
+    Container
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Feels like {{feelsLike}}</h1>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      weatherData: fetch('https://api.openweathermap.org/data/2.5/weather?zip=11418,us&units=imperial&appid=c93956b233247d73e54794d4d5d1d5d8')
+        .then(async res => {
+          let json = await res.json()
+          return json
+        }).catch(e => console.log(e.message)),
+      feelsLike: ''
+    }
+  },
+  created () {
+    this.weatherData.then(wData => {
+      this.feelsLike = wData.main.feels_like
+    })
+    // const weatherData = fetch('https://api.openweathermap.org/data/2.5/weather?zip=11418,us&appid=c93956b233247d73e54794d4d5d1d5d8')
+    //                           .then( async res => {
+    //                             return await res.json()
+    //                           }).catch(e => console.log(e.message))
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
